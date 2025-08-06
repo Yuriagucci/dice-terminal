56781d844db9566a2c7db1610a0e83bde51f1830
Yuriagucci <mokouawesome0028@gmail.com>
2025-08-07 04:14:28 +0800

    Added a new feature for dice rolling

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
-import customTheme from './theme'; // Import your custom theme
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
